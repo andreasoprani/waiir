9b25eb5e8e2f9fc9cf97f3d27c7b3c4bf43de7f4
andreasoprani <andrea.soprani96@gmail.com>
2025-07-26 18:38:44 +0200

    Chapter 2: RPPL as main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::io::stdin;
+
+use waiir::Parser;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, this is the Monkey programming language!");
+    println!("Feel free to type in commands");
+    loop {
+        let mut buf = String::new();
+        stdin().read_line(&mut buf).unwrap();
+        let mut line_parser = Parser::init(buf.as_str());
+        let program = line_parser.parse_program();
+        for stmt in program.statements {
+            println!("{:?}", stmt);
+        }
+    }
 }
